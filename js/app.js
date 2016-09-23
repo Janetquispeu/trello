dc49155083be55a1e54a0c721ca67b0e85773ed5
smiling girl.. sonrie   siempre  en la   vida <smiling girl.. sonrie   siempre  en la   vida>
2016-09-23 01:06:41 -0500

    version 4

--- js/app.js
+++ js/app.js
@@ -10,18 +10,17 @@ window.addEventListener("load",function(){
 	}
 
 	function aparecerFormulario(){
-		var contenedor=document.getElementById("contenedor");
-		var form=document.createElement("form");
+		var form=document.getElementById("form");
 		var inputText=document.createElement("input");
 		var boton=document.createElement("button");
 		var btnRemove=document.createElement("button")
 
-		contenedor.appendChild(form);
 		form.appendChild(inputText);
 		form.appendChild(boton);
 		form.appendChild(btnRemove);
 
 		form.classList.add("fondoForm")
+		form.style.background="#E2E4E6"; 
 		boton.textContent= "Save";
 		inputText.classList.add("inputText");
 		inputText.setAttribute("placeholder","Add an List...")
@@ -35,6 +34,7 @@ window.addEventListener("load",function(){
 			addList();
 
 		});
+		
 
 		function imprimir(){
 			var inputValor=inputText.value;
@@ -43,11 +43,15 @@ window.addEventListener("load",function(){
 			var divEnlace=document.createElement("div");
 			var divList=document.createElement("div");
 			var cardEnlace=document.createElement("a");
+
 			contenedor.insertBefore(divContent,form);
 			divContent.appendChild(divList);
 			divContent.appendChild(divEnlace);
 			divEnlace.appendChild(cardEnlace);
+
 			divList.textContent=inputValor;
+			divList.value="";
+
 			divContent.classList.add("createList");
 			divList.classList.add("divClass");
 			divEnlace.classList.add("divEnlace");
@@ -57,14 +61,15 @@ window.addEventListener("load",function(){
 		}
 
 		function desapareceFormulario(){
-			form.style.display="none";
+			inputText.style.display="none";
+			boton.style.display="none";
+			btnRemove.style.display="none";
+			form.style.background="none";
+
 			}
 		function addList(){
-			var addList=document.createElement("div");
-			contenedor.appendChild(addList);
-			addList.textContent="Add an card...";
-			addList.classList.add("addList");
-			addList.setAttribute("id","addList");
+			var addList=document.getElementById("addList");
+			addList.style.display="block";
 
 		}
 	}			
